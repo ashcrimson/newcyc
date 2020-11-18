4f98dddb29fefb1dc122d413c460f5d07ffc7c9a
Felipe Pino <felipepinoredes@gmail.com>
2020-11-18 20:13:30 -0300

    detalle contrato

--- pages/contratos/contratosShow/view.php
+++ pages/contratos/contratosShow/view.php
@@ -15,7 +15,12 @@ class ViewContratos {
 
 		if(isset($_GET["id"])){
             $contrato = $model->get();
-        }
+		}
+		
+		$data = $model->get();
+
+        //Forman la tabla
+		$listado = $data[0];
 
 
 
@@ -46,8 +51,48 @@ class ViewContratos {
 
 		<div class="card mb-3">
 			<div class="card-header">
-                Aca se muestra el contrato con sus detalles
-                <p>Objeto: <?=$contrato['OBJETO_CONTRATO']?></p>
+			<div class="table-responsive table-sm -md -lg -x">
+				<table class="table table-bordered"  class="table-sm w-25" id="dataBitacoras" width=100% cellspacing="0">
+					<thead>
+						<tr>
+							<th>Código</th>
+							<th>DESC_PROD_SOLI</th>
+							<th>CANTIDAD TOTAL</th>
+							<th>PRECIO_U_BRUTO</th>
+							<th>GRUPO</th>
+							<th>PRESENTACION_PROD_SOLI</th>
+							<th>F_F</th>
+							<th>DESC_TEC_PROD_OFERTADO</th>
+							<th>U_ENTREGA_OFERENTE</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+						foreach ($listado as $index => $contrato) {
+							// foreach ($documentos as $documento) {
+							?>
+						<?php foreach($contrato["DETALLES"] as  $detalle){?>
+								<tr>
+								
+									<td> <?= $detalle["CODIGO"]; ?></td>
+									<td> <?= $detalle["DESC_PROD_SOLI"]; ?></td>
+									<td> <?= $detalle["CANTIDAD_TOTAL"]; ?></td>
+									<td> <?= $detalle["PRECIO_U_BRUTO"]; ?></td>
+									<td> <?= $detalle["GRUPO"]; ?></td>
+									<td> <?= $detalle["PRESENTACION_PROD_SOLI"]; ?></td>
+									<td> <?= $detalle["F_F"]; ?></td>
+									<td> <?= $detalle["DESC_TEC_PROD_OFERTADO"]; ?></td>
+									<td> <?= $detalle["U_ENTREGA_OFERENTE"]; ?></td>
+								</tr>
+						<?php }?>
+						
+						<?php
+                        // }
+                    }
+                    ?>
+					</tbody>
+				</table>
+                </div>
 			</div>
 		</div>
 
