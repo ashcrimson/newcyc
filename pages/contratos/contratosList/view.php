28c4bca8d426831d86bc59ed410f6c1fd1c16422
unknown <felipepinoredes@gmail.com>
2021-02-11 15:11:52 -0300

    cambios tabla

--- pages/contratos/contratosList/view.php
+++ pages/contratos/contratosList/view.php
@@ -51,17 +51,7 @@ class ViewContratos {
         
     }
 
-    .table-responsiva{
-        width: 230%;
-    }
 
-    .table {
-        width: 200%;
-    }
-
-    
-
-   
     </style>
  
     <!-- Breadcrumbs-->
@@ -73,12 +63,12 @@ class ViewContratos {
     </ol>
 
     <!-- DataTables Example --> 
-    <div class="card mb-3">
-        <div class="card-header">
+    <div class="container">
+        <div >
             <form method="get" class="form-horizontal" action="<?=base("/contratos/")?>">
                 
                 <div class="row">
-                    <div class="col-6">
+                    <div class="col-md-6 col-sm-12">
                         <label>Proveedor</label>
                             <div>
                                 <select name="rut_proveedor" class="selectpicker selectField"  placeholder='Seleccione RUT Proveedor' data-live-search='true'>
@@ -97,7 +87,7 @@ class ViewContratos {
                             </div>
                     </div>
  
-                    <div class="col-3">
+                    <div class="col-md-3 col-sm-12">
                         <label>ID Contrato</label>
                             <div>
                                 <select name="id_contrato" class="selectpicker selectField" placeholder='Seleccione Contrato' data-live-search='true'>
@@ -118,7 +108,7 @@ class ViewContratos {
                             </div>
                     </div>
 
-                    <div class="col-3">
+                    <div class="col-md-3 col-sm-12">
                         <label>Cargo Admin. Técnico</label>
                             <div>
                                 <select name="cargos" class="selectpicker selectField" placeholder='Seleccione Cargo' data-live-search='true'>
@@ -144,7 +134,7 @@ class ViewContratos {
 
                     
 
-                    <div class="col-3 mt-3">
+                    <div class="col-md-3 col-sm-12 mt-3">
                         <label>Vigencia Contrato</label>
                         <div>
                             <select name="vigencia" class="selectpicker selectField" placeholder='Seleccione Vigencia' data-live-search='true'>
@@ -155,7 +145,7 @@ class ViewContratos {
                         </div> 
                     </div>
 
-		            <div class="col-3 mt-3">
+		            <div class="col-md-3 col-sm-12 mt-3">
                         <label>Licitación</label>
                         <div>
                             <select name="licitacion" class="selectpicker selectField" placeholder='Seleccione Licitación' data-live-search='true'>
@@ -177,7 +167,7 @@ class ViewContratos {
                         </div>
                     </div>
 
-                    <div class="col-3 mt-3">
+                    <div class="col-md-3 col-sm-12 mt-3">
                         <label>Objeto Contrato</label>
                         <div>
                             <select name="objeto" class="selectpicker selectField" placeholder='Seleccione Objeto' data-live-search='true'>
@@ -223,38 +213,38 @@ class ViewContratos {
             </form>
         </div>
 
-        <div class="card-body">
-
-
-            <div class="table-responsive">
-                <table class="table table-bordered table-condensed table-fixed" id="dataTable" >
-                    <thead>
+        <div class="container-fluid">
+             
+            <table class="table table-hover table-responsive">
+                <thead>
                     <tr >
+                        <th >ID Contrato</th>
                         <th >Rut Proveedor</th>
                         <th >Razón Social Proveedor</th>
-                        <th >ID Contrato</th>
+                        
                         <th >Licitación</th>
-                        <th >Moneda</th>
+                        <!-- <th >Moneda</th> -->
                         <!-- <th>Precio</th> -->
                         
                         <!-- <th>Restante</th> -->
-                        <th>Cargo</th>
-                        <th>Fecha inicio contrato</th>
-                        <th>Fecha termino contrato</th>
-                        <th>Fecha último acto administrativo</th>
-                        <th>Objeto del contrato</th>
-                        <th>N° Boleta Garantía</th>
-                        <th>Monto</th>
-                        <th>Valor CLP</th>
+                        <!-- <th>Cargo</th> -->
+                        <!-- <th>Fecha inicio contrato</th> -->
+                        <!-- <th>Fecha termino contrato</th> -->
+                        <!-- <th>Fecha último acto administrativo</th> -->
+                        <!-- <th>Objeto del contrato</th> -->
+                        <!-- <th>N° Boleta Garantía</th> -->
+                        <!-- <th>Monto</th> -->
+                        <!-- <th>Valor CLP</th> -->
                         <th>Saldo</th>
                     
-                        <th>Fecha Alerta de Vencimiento</th>
-                        <th>Fecha Vencimiento Boleta</th>
-                        <th>Adjunto</th>
+                        <!-- <th>Fecha Alerta de Vencimiento</th> -->
+                        <!-- <th>Fecha Vencimiento Boleta</th> -->
+                        <!-- <th>Adjunto</th> -->
                         
                         <th>Bitácora</th>
                         <th>Editar</th>
                         <th>Detalle Contrato</th>
+                        <th>Más información</th>
                         <?php if($authUser['ID_PERMISO'] == 2) {
                         ?>
                         <th>Asignar Contrato</th>
@@ -269,35 +259,35 @@ class ViewContratos {
                         ?>
                         <tr> 
 
-
-                            <td style="width:7%;"><?= $contrato["RUT_PROVEEDOR"]; ?></td>
-                            <td style="width:7%;"><?= $contrato["RAZON_SOCIAL"]; ?></td>
-                            <td style="width:1%;"><?= $contrato["TIPO"] ."-". $contrato["ID_CONTRATO"]; ?></td>
-                            <td style="width:6%;"><?= $contrato["NRO_LICITACION"]; ?></td>
-                            <td style="width:1%;"><?= $contrato["NOMBRE_MONEDA"]; ?></td>
+                            <td ><?= $contrato["TIPO"] ."-". $contrato["ID_CONTRATO"]; ?></td>
+                            <td ><?= $contrato["RUT_PROVEEDOR"]; ?></td>
+                            <td style="width:1%;"><?= $contrato["RAZON_SOCIAL"]; ?></td>
+                            
+                            <td ><?= $contrato["NRO_LICITACION"]; ?></td>
+                            <!-- <td style="width:1%;"><?= $contrato["NOMBRE_MONEDA"]; ?></td> -->
                             <!-- <td><?= $contrato["PRECIO"]; ?></td> -->
                             
                             <!-- <td><?= $contrato["RESTANTE"];?></td> -->
-                            <td style="width:2%;"><?= $contrato["NOMBRE_CARGO"];?></td>
-                            <td style="width:3%;"><?= $contrato["FECHA_INICIO"]; ?></td>
-                            <td style="width:3%;"><?= $contrato["FECHA_TERMINO"]; ?></td> 
-                            <td style="width:2%;"><?= $contrato["FECHA_APROBACION"]; ?></td>
-                            <td style="width:2%;"><?= $contrato["OBJETO_CONTRATO"]; ?></td>
-                            <td style="width:1%;"><?= $contrato["NRO_BOLETA_GARANTIA"]; ?></td>
-                            <td style="width:1%;">$<?= number_format($contrato["MONTO"], 2, ',', '.') ?></td>
-                            <td style="width:1%;">$<?= number_format($contrato["MONTO"] * $contrato["EQUIVALENCIA"], 2, ',', '.'); ?></td>
-                            <td style="width:1%;">$<?= number_format($contrato["SALDO"], 2, ',', '.') ?></td>
+                            <!-- <td style="width:2%;"><?= $contrato["NOMBRE_CARGO"];?></td> -->
+                            <!-- <td style="width:3%;"><?= $contrato["FECHA_INICIO"]; ?></td> -->
+                            <!-- <td style="width:3%;"><?= $contrato["FECHA_TERMINO"]; ?></td>  -->
+                            <!-- <td style="width:2%;"><?= $contrato["FECHA_APROBACION"]; ?></td> -->
+                            <!-- <td style="width:2%;"><?= $contrato["OBJETO_CONTRATO"]; ?></td> -->
+                            <!-- <td style="width:1%;"><?= $contrato["NRO_BOLETA_GARANTIA"]; ?></td> -->
+                            <!-- <td style="width:1%;">$<?= number_format($contrato["MONTO"], 2, ',', '.') ?></td> -->
+                            <!-- <td style="width:1%;">$<?= number_format($contrato["MONTO"] * $contrato["EQUIVALENCIA"], 2, ',', '.'); ?></td> -->
+                            <td >$<?= number_format($contrato["SALDO"], 2, ',', '.') ?></td>
                             <!-- <td style="width:1%;">$<?= number_format($contrato["MONTO"] - $contrato["TOTAL"], 2, ',', '.'); ?></td> -->
 
-                            <td style="width:2%;"><?= $contrato["FECHA_ALERTA_VENCIMIENTO"]; ?></td>
-                            <td style="width:2%;"><?= $contrato["FECHA_VENCIMIENTO_BOLETA"]; ?></td>
-                            <td style="width:1%;">
+                            <!-- <td style="width:2%;"><?= $contrato["FECHA_ALERTA_VENCIMIENTO"]; ?></td> -->
+                            <!-- <td style="width:2%;"><?= $contrato["FECHA_VENCIMIENTO_BOLETA"]; ?></td> -->
+                            <!-- <td style="width:1%;">
                                 <a href="<?= base()."/archivo/download?id=".$contrato['NRO_DOCUMENTO'] ?>" target="_blank">
                                     <?= $contrato["NOMBRE_DOCUMENTO"] ?>
                                 </a>
-                            </td>
+                            </td> -->
 
-                            <td style="width:2%;">
+                            <td >
                                 <a href="<?=base("/contratos/bitacora/show?id=").$contrato["ID_CONTRATO"];?>" class="btn btn-sm btn-success btn-xs">
                                     <i class="fa fa-pencil-alt"></i> Bitacoras
                                 </a>
@@ -309,98 +299,100 @@ class ViewContratos {
 
 
                             </td>
-                            <td style="width:2%;">
+                            <td >
                                 <a href="<?=base("/contratos/new?id=").$contrato["ID_CONTRATO"];?>" class="btn btn-sm btn-primary btn-xs">
                                     <i class="fa fa-pencil-alt"></i> Editar
                                 </a>
                             </td>
-                            <td style="width:2%;">
+                            <td >
                                 <!-- <a href="#" data-target="#miModal<?=$index;?>" data-toggle="modal" class="btn btn-sm btn-success btn-xs">
                             <i class="fa fa-book-open"></i> Detalle</a>
  -->
-                                <a href="<?=base("/contratos/show?id=").$contrato["ID_CONTRATO"];?>" class="btn btn-sm btn-success btn-xs">
-                                    <i class="fa fa-pencil-alt"></i> Detalles
-                                </a>
+                <a href="<?=base("/contratos/show?id=").$contrato["ID_CONTRATO"];?>" class="btn btn-sm btn-success btn-xs">
+                    <i class="fa fa-pencil-alt"></i> Detalles
+                </a>
 
-                            </td>
+            </td>
+            <td >
+                <a href="<?=base("/contratos/new?id=").$contrato["ID_CONTRATO"];?>" class="btn btn-sm btn-primary btn-xs">
+                    <i class="fa fa-eye"></i> Ver
+                </a>
+            </td>
                             
 
-                            <?php if($authUser['ID_PERMISO'] == 2) {
-                            ?>
-                            <td style="width:2%;">
-                            <?php
-                                if(!$contrato['ASIGNADO']) {
-                                    ?>
-
-                                    <a href="#" class="btn btn-sm btn-warning btn-xs" data-target="#modalAsigna<?=$index;?>" data-toggle="modal">
-                                        <i class="fa fa-plus-square"></i> Asignar
-                                    </a>
-
-                                    <div class="modal fade" id="modalAsigna<?=$index;?>">
-                                        <div class="modal-dialog">
-                                            <div class="modal-content">
-                                                <form method="post" action="<?=base("/contratos/asignar/store");?>">
-                                                    <div class="modal-header">
-                                                        <h5 class="modal-title">
-                                                            Asignar Contrato
-                                                        </h5>
-                                                    </div>
-
-
-                                                    <div class="modal-body">
-                                                        <div class="table-responsive table-sm -md -lg -x">
-                                                            <?php
-                                                                foreach ($contrato['AREAS'] as $i => $area) {
-
-                                                                    ?>
-                                                                    <input type="radio" id="radio<?=$i?>" name="area" value="<?=$area['ID_AREA'];?>">
-                                                                    <label for="radio<?=$i?>"><?=$area['AREA'];?></label><br>
-                                                                    <?php
-                                                                }
-                                                            ?>
-
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-default" data-dismiss="modal">
-                                                            Cancelar
-                                                        </button>
-                                                        <button type="submit" class="btn btn-success">
-                                                            <i class="fa fa-floppy-o"></i>
-                                                            Guardar
-                                                        </button>
-                                                        <input type="hidden" name="asignar" value="1">
-                                                        <input type="hidden" name="id_contrato" value="<?=$contrato['ID_CONTRATO']?>">
-                                                    </div>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <?php
-                                } else {
-                                    echo "asignado";
-                                }
-                            } 
-                            else {
-                                echo "";
-                                } 
-                            ?>
-                            </td>
-
-
-                        </tr>
-                        <?php
-                        // }
-                    }
+                    <?php if($authUser['ID_PERMISO'] == 2) {
                     ?>
+                    <td >
+                    <?php
+                        if(!$contrato['ASIGNADO']) {
+                            ?>
 
-                    </tbody>
-                </table>
-            </div>
+                            <a href="#" class="btn btn-sm btn-warning btn-xs" data-target="#modalAsigna<?=$index;?>" data-toggle="modal">
+                                <i class="fa fa-plus-square"></i> Asignar
+                            </a>
 
-            
+                            <div class="modal fade" id="modalAsigna<?=$index;?>">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <form method="post" action="<?=base("/contratos/asignar/store");?>">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">
+                                                    Asignar Contrato
+                                                </h5>
+                                            </div>
+
+
+                                            <div class="modal-body">
+                                                <div class="table-responsive table-sm -md -lg -x">
+                                                    <?php
+                                                        foreach ($contrato['AREAS'] as $i => $area) {
+
+                                                            ?>
+                                                            <input type="radio" id="radio<?=$i?>" name="area" value="<?=$area['ID_AREA'];?>">
+                                                            <label for="radio<?=$i?>"><?=$area['AREA'];?></label><br>
+                                                            <?php
+                                                        }
+                                                    ?>
+
+                                                </div>
+                                            </div>
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">
+                                                    Cancelar
+                                                </button>
+                                                <button type="submit" class="btn btn-success">
+                                                    <i class="fa fa-floppy-o"></i>
+                                                    Guardar
+                                                </button>
+                                                <input type="hidden" name="asignar" value="1">
+                                                <input type="hidden" name="id_contrato" value="<?=$contrato['ID_CONTRATO']?>">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                        } else {
+                            echo "asignado";
+                        }
+                    } 
+                    else {
+                        echo "";
+                        } 
+                    ?>
+                    </td>
+
+
+                </tr>
+                <?php
+                // }
+            }
+            ?>
+            </table>
         </div>
+
+        
 
         <div class="card-footer">
             <?php
@@ -408,6 +400,8 @@ class ViewContratos {
             ?>
         </div>
     </div>
+
+    
 
     <script src="<?=base();?>/assets/assets/frontend/js/jquery-3.3.1.js"></script>
     <script src="<?=base();?>/assets/assets/frontend/js/selectize.js"></script>
